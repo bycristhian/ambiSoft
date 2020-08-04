d8e7ec84dc0c96351d2eca392d9110590c610ef0
bycristhian <cristhian.2002.0@gmail.com>
2020-08-04 15:16:37 -0500

    Updates

---
+++ precache-manifest.76f62dfa197dda995a5a1b63045112e7.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8279b19994c0f9b726e0d57f3e3ad75",
+    "url": "/ambiSoft/index.html"
+  },
+  {
+    "revision": "d8b89b8a08319844a7f1",
+    "url": "/ambiSoft/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "2241fc9c975717d9501a",
+    "url": "/ambiSoft/static/css/main.9cca05dd.chunk.css"
+  },
+  {
+    "revision": "d8b89b8a08319844a7f1",
+    "url": "/ambiSoft/static/js/2.4e180c38.chunk.js"
+  },
+  {
+    "revision": "b4ee14f1b0eb12541a4fed8ba73fd856",
+    "url": "/ambiSoft/static/js/2.4e180c38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2241fc9c975717d9501a",
+    "url": "/ambiSoft/static/js/main.86651c6d.chunk.js"
+  },
+  {
+    "revision": "7e4ec7b063c09363105d",
+    "url": "/ambiSoft/static/js/runtime-main.ce6d28e1.js"
+  },
+  {
+    "revision": "7c27c6a066d874c987fae1e74987b681",
+    "url": "/ambiSoft/static/media/amarilla.7c27c6a0.png"
+  },
+  {
+    "revision": "04ee80d3108ffde8ca8c52a730e8a9b7",
+    "url": "/ambiSoft/static/media/azul.04ee80d3.png"
+  },
+  {
+    "revision": "0e7c54e01b19b86363d50e67dfda3153",
+    "url": "/ambiSoft/static/media/fondo1.0e7c54e0.png"
+  },
+  {
+    "revision": "687463bc41393bc81798789d3ac0fd9f",
+    "url": "/ambiSoft/static/media/gris.687463bc.png"
+  },
+  {
+    "revision": "7f2dd67cf75e403f1186dd8e3a5934ae",
+    "url": "/ambiSoft/static/media/plant.7f2dd67c.jpg"
+  },
+  {
+    "revision": "b760d605ecdd0b27333995172afc940d",
+    "url": "/ambiSoft/static/media/rojo.b760d605.png"
+  },
+  {
+    "revision": "66b1001ef6f4bcec563ce6ed38297542",
+    "url": "/ambiSoft/static/media/slider1.66b1001e.jpg"
+  },
+  {
+    "revision": "52eba892f094ead61e152f3dd1cdd6e8",
+    "url": "/ambiSoft/static/media/slider10.52eba892.jpg"
+  },
+  {
+    "revision": "086b98cd4e85fbdc7917283204da88b5",
+    "url": "/ambiSoft/static/media/slider4.086b98cd.jpg"
+  },
+  {
+    "revision": "7c0bd93a7935c64642e247c7ec8b0ae9",
+    "url": "/ambiSoft/static/media/sound_game.7c0bd93a.mp3"
+  },
+  {
+    "revision": "2a5221f0a4023b668fbb16fba427df0d",
+    "url": "/ambiSoft/static/media/sound_incorrect.2a5221f0.mp3"
+  },
+  {
+    "revision": "6f0179188b334cbd8e810f975c53c6e8",
+    "url": "/ambiSoft/static/media/sound_win.6f017918.mp3"
+  },
+  {
+    "revision": "a0794a7f2601829a34ac79a118fb3a53",
+    "url": "/ambiSoft/static/media/verde.a0794a7f.png"
+  }
+]);
