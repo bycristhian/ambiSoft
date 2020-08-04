09549d91ec07249de4576be42683911e45f25e04
bycristhian <cristhian.2002.0@gmail.com>
2020-08-04 16:21:56 -0500

    Se configura los derechos de autor

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,13 @@
 
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 const Footer = (props) => (
 
     <footer className="page-footer font-small blue" style={{position: `${props.styles}`}}>
 
-        <div class="footer-copyright text-center py-3">© 2020 Copyright:
-            <Link to="/"> AmbiSoft</Link>
+        <div className="footer-copyright text-center py-3">
+            © 2020 Copyright:<a href="https://github.com/bycristhian"> <b>@bycristhian</b></a>
+            <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
         </div>
 
     </footer>
